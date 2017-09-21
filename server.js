b055bfd762d84c30580e9adfa9a1ad4ca1e7a300
Brian Jang <brian.jang91@gmail.com>
2017-09-20 22:09:09 -0400

    Add basic request

--- server.js
+++ server.js
@@ -11,11 +11,18 @@ var app = express();
 var router = express.Router();
 
 // set port;
-var port = process.env.API_PORT || 3001;
+var port = process.env.API_PORT || 3000;
 
 // use body parser to pick out JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+router.get("/", function(req, res) {
+  res.json({ message: "GETTING STUFF DONE!"});
+})
+
+app.use("/api", router);
+
 
 app.listen(port, function() {
   console.log(`Successfully running on ${port}`)
