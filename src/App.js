38422723fa2dceeeb6c2fa797c9f471555872b3c
Brian Jang <brian.jang91@gmail.com>
2017-09-22 19:58:43 -0400

    Add more stuff to App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <h1>I should be displayed if things work fine</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
     )
   }
 }
