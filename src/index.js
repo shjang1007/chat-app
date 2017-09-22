2a838ed61411bd72db4cae1d59664dd2f8f99b97
Brian Jang <brian.jang91@gmail.com>
2017-09-22 19:41:31 -0400

    Use ract router v4 instead of 3 on this

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// Import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+// Import components
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={ browserHistory }>
+    <Route path="/" component={ App }></Route >
+  </Router>,
+  document.getElementById('root')
+);
 // registerServiceWorker();
