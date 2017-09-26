10ff6674ff6971e4582cb33fa31514c539095d4e
Brian Jang <brian.jang91@gmail.com>
2017-09-26 19:42:28 -0400

    Change authorship to the correct names

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-      </ul>
+      <div>This is where it all begins</div>
     )
   }
 }
