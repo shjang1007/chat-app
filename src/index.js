91b96c5da1cf35e91fceae50f15fa61a13c1ec7b
Brian Jang <brian.jang91@gmail.com>
2017-09-26 19:54:04 -0400

    Add successful routing through browserrouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 // Import components
 import App from './app';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <Route path="/hello" component={ App }></Route>
   </BrowserRouter>,
   document.getElementById('root')
 );
