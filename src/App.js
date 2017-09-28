a5743fb227f921147a290b7d86d18f92eb34202f
Brian Jang <brian.jang91@gmail.com>
2017-09-27 20:06:27 -0400

    Add axios get request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    this.state = {
+      data : [];
+    }
+  }
+
+  fetchChatHistory() {
+    axios.get(this.props.url).then(res => {
+      this.setState({ data: res.data });
+  })
+}
+  }
+
   render() {
     return (
       <section>
