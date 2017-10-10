cc29c44f9ccdf33c5e503b5223fb33f5b8c7edb5
Brian Jang <brian.jang91@gmail.com>
2017-10-09 23:48:15 -0400

    Add possible modal structure

---
+++ src/modal.js
@@ -0,0 +1,19 @@
+// import dependencies
+import React, { Component } from "recat";
+
+// What good is website without modals?
+class Modal extends Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return(
+      <section>
+        Why doesn't it auto populate?
+      </section>
+    );
+  }
+}
+
+export default Modal;
