584f6defab000afacf074f7d14f93fc875f76bff
Brian Jang <brian.jang91@gmail.com>
2017-10-10 19:16:37 -0400

    Add comments that may prove to be useful in the near future

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ mongoose.Promise = global.Promise;
 
 // Import Schema
 var Dummy = require("./models/dummy");
-var User = require("./models/user");
 
 // Create Dummy models
 // var newDummy = Dummy({
@@ -28,45 +27,56 @@ var User = require("./models/user");
 //
 // newDummy.save((err) => {
 //   if(err) throw err;
-// 
+//
 //   console.log("Dummy Created");
 // });
 
-Dummy.find({}, (err, dummies) => {
-  if(err) throw err;
+// Dummy.find({}, (err, dummies) => {
+//   if(err) throw err;
+//
+//   console.log(dummies);
+// })
 
-  console.log(dummies);
+// Dummy.find({}, (err, dummies) => {
+//   dummies.forEach((dummy) => {
+//     dummy.remove((err) => {
+//       if(err) throw err;
+//
+//       console.log("deleted");
+//     })
+//   })
+// })
+
+// create App and Router
+var app = express();
+var router = express.Router();
+
+// set port;
+var port = process.env.API_PORT || 4000;
+
+// use body parser to pick out JSON data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Middleware to handle requests
+// Prior to express v4, things did not run in the order it was wrote
+router.use(function(req, res, next) {
+  // testing for now
+  // possible validation point?
+  console.log("Transfering @ middleware station");
+  next(); // this is the key to middleware
+});
+
+
+// Will act as tester to see if our routes are working properly
+router.get("/", function(req, res) {
+  res.json({ message: "welcome to the world where APIs happen"});
 })
 
-// // create App and Router
-// var app = express();
-// var router = express.Router();
-//
-// // set port;
-// var port = process.env.API_PORT || 4000;
-//
-// // use body parser to pick out JSON data
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-//
-// // Middleware to handle requests
-// router.use(function(req, res, next) {
-//   // testing for now
-//   console.log("I am inside my middleware yoohoo")
-//   next(); // this is the key to middleware
-// });
-//
-//
-// // Will act as tester to see if our routes are working properly
-// router.get("/", function(req, res) {
-//   res.json({ message: "welcome to the world where APIs happen"});
-// })
-//
-// // API set to handle requests
-// app.use("/api", router);
-//
-//
-//
-// app.listen(port, function() {
-//   console.log(`Port ${port} is where we get stuff done!`)
-// });
+// handle API request with /api prefix
+app.use("/api", router);
+
+
+app.listen(port, function() {
+  console.log(`Port ${port} is where we get stuff done!`)
+});
