73239588cd41876ba1bc89ef1bf89fc527cc7785
Brian Jang <brian.jang91@gmail.com>
2017-10-09 23:42:26 -0400

    Add more db columns on user

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  token: String
+  token: String,
+  name: String
 })
 
 module.export = mongoose.model("User", UserSchema);
