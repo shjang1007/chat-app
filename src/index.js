6c85a4f04960f4e8ec9d5fbb11854fabf1f0825a
Brian Jang <brian.jang91@gmail.com>
2017-11-07 18:21:50 -0500

    Add comments to remember steps needed to be taken after switching app to webpack babel environment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from './app';
 import HomePage from "./homepage";
 
+// Determine if browser router is indeed correct router to use here
+// Also, further read on Switch to see if I can re-route without using Switch
+
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
