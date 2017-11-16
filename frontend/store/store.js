201ce18f94401a819f9369ef9c31b4693ee30aaa
Brian Jang <brian.jang91@gmail.com>
2017-11-15 19:08:59 -0500

    Setup store with middleware

---
+++ frontend/store/store.js
@@ -0,0 +1,14 @@
+// import dependencies
+import { createStore, applyMiddleware } from "redux";
+import thunk from "../middleware/thunk";
+import rootReducer from "../reducers/root_reducer";
+
+const configureStore = (preloadedState = {}) => {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk)
+  );
+};
+
+export default configureStore;
