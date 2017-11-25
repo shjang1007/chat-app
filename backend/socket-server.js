497a3e4ea67494f38768ec9a981af6c320f2c1b9
Brian Jang <brian.jang91@gmail.com>
2017-11-24 21:50:10 -0500

    Add assignGuestName method

--- backend/socket-server.js
+++ backend/socket-server.js
@@ -1,13 +1,24 @@
 import Socket from "socket.io";
 
 // Trying to determine what below variables are used for
-let chat
-let guestNumber = 1
-
-const nickNames = {}
-let namesUsed = []
-const currentRoom = {}
+let chat;
+let guestNumber = 1;
+const nickNames = {};
+let namesUsed = [];
+const currentRoom = {};
 
 class SocketServer {
+  // Method to tell server to add more guests to socket server
+  assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    const id = `${guestNumber}`;
+    nickNames[socket.id] = id;
+    socket.emit("nameResult", {
+      success: true,
+      id
+    });
+
+    namesUsed.push(id);
+    return guestNumber + 1;
+  }
 
 }
