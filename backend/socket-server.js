7754ed32e4a6b5568006c4800c30c936e6aa1485
Brian Jang <brian.jang91@gmail.com>
2017-11-24 21:39:01 -0500

    Add variables to be used for socket server portion of the application

--- backend/socket-server.js
+++ backend/socket-server.js
@@ -1,5 +1,13 @@
 import Socket from "socket.io";
 
+// Trying to determine what below variables are used for
+let chat
+let guestNumber = 1
+
+const nickNames = {}
+let namesUsed = []
+const currentRoom = {}
+
 class SocketServer {
-  
+
 }
