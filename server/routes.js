ebe418096af834201d198eaa9bef214b1bda8686
Brian Jang <brian.jang91@gmail.com>
2017-11-29 20:21:09 -0500

    Setup routes for messages, needs to configure message controller next

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,20 @@
+// Import dependencies
 const express = require("express");
 const router = express.Router();
+
+// Import controller files
+const messageController = require("./controllers/message-controller");
 
 router.route("/users")
 .post((req, res) => {
   }
 )
+
+router.route("/messages")
+  .get(messageController.getMessages)
+  .post(messageController.addMessages)
+  .patch(messageController.updateMessages);
+
+router.route("/messages/:id")
+  .get(messageController.getMessage)
+  .delete(messageController.deleteMessage);
