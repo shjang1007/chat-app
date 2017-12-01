634f975e40aebdfcabf628be9eabd7d42063dd1b
Brian Jang <brian.jang91@gmail.com>
2017-11-30 22:23:58 -0500

    Add update message

--- server/controllers/messages-controller.js
+++ server/controllers/messages-controller.js
@@ -8,16 +8,30 @@ const Message = require("../models/message");
 export const createMessage = (req, res) => {
   const newMessage = new Message(req.body);
   newMessage.save((err, message) => {
-    if(err) return res.send(err);
+    if(err) res.send(err);
 
     return res.json({
       "success": "true",
       "message": "Successfully created message",
       message
-    })
-  })
-}
+    });
+  });
+};
 
 // updateMessage
+export const updateMessage = (req, res) => {
+  // first param is to lookup, second param is to provide content to update, third param is to return the updated obj.
+  Message.findOneAndUpdate({ _id: req.body.id }, req.body, { new: true },
+    (err, message) => {
+      if(err) res.send(err);
+
+      return res.json({
+        "success": "true",
+        "message": "Successfully upated message",
+        message
+      });
+    });
+};
+
 
 // deleteMessage
