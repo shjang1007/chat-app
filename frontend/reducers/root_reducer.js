5fc411d178f2b5f23f3549433f21ae083588adbe
Brian Jang <brian.jang91@gmail.com>
2017-12-02 16:19:40 -0500

    Add message reducer to be combined in root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,9 @@
+// import combineReducers to combine all the reducers without having to import each reducer separetly
 import { combineReducers } from "redux";
 
+// import reducers to be combined
+import messageReducer from "./message_reducer";
 
 export default combineReducers({
-  // session: sessionReducer,
+  messages: messageReducer
 });
