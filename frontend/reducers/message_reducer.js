ead69e283bd03d660804f8adaaea3b4fb5569b93
Brian Jang <brian.jang91@gmail.com>
2017-12-02 16:12:18 -0500

    Add message reducer

---
+++ frontend/reducers/message_reducer.js
@@ -0,0 +1,24 @@
+import { RECEIVE_MESSAGE, DELETE_MESSAGE } from "../actions/message_actions";
+
+const _initialState = {};
+
+const messageReducer = (oldState = _initialState, action) => {
+  // Keep oldState immutable
+  Object.freeze(oldState);
+
+  let newState;
+
+  switch (action.type) {
+    case RECEIVE_MESSAGE:
+      newState = Object.assign(oldState, action.message);
+      return newState;
+    case DELETE_MESSAGE:
+      newState = Object.assign(oldState, action.message);
+      delete newState[action.messageId];
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default messageReducer;
