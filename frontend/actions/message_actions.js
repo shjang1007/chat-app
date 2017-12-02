51d774f1d2901f138d35ad7f1b6ca8df7bcc0454
Brian Jang <brian.jang91@gmail.com>
2017-12-02 15:25:46 -0500

    Add message action for redux cycle

---
+++ frontend/actions/message_actions.js
@@ -0,0 +1,33 @@
+// import ajax request to the server
+import * as messageApiUtil from "../util/message_api_util";
+
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+export const DELETE_MESSAGE = "DELETE_MESSAGE";
+
+export const createMessage = (message) => (dispatch) => {
+  return messageApiUtil.createMessage(message).then(
+    (message) => dispatch(receiveMessage(message));
+  );
+};
+
+export const deleteMessage = (id) => (dispatch) => {
+  return messageApiUtil.deleteMessage(id).then(
+    (id) => dispatch(deleteMessage(id));
+  )
+}
+
+export const updateMessage
+
+export const receiveMessage = (message) => {
+  return({
+    type: RECEIVE_MESSAGE,
+    message
+  });
+};
+
+export const deleteMessage = (id) => {
+  return({
+    type: DELETE_MESSAGE,
+    id
+  });
+};
