9544385458e13a01748c89058929f8507af8ddd8
Brian Jang <brian.jang91@gmail.com>
2017-12-01 21:12:01 -0500

    Fix babelRegister error

--- server/server-hooked.js
+++ server/server-hooked.js
@@ -1,3 +1,9 @@
 // use babel register package to transpile ES6 to ES5
-require("babel-register")
-require("./server")
+const babelRegister = require("babel-register");
+
+babelRegister({
+  presets: ["env"]
+});
+
+// Any modules imported will be compiled
+require("./server");
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
 "use strict"
 
 // Import dependencies
-const express = require("express");
+import express from "express";
+// const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
