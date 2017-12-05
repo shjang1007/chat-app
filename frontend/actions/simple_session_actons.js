c0a8f03e66fe32814c2963660eac4cfd41b2ef1b
Brian Jang <brian.jang91@gmail.com>
2017-12-05 18:01:42 -0500

    Add action logic

--- frontend/actions/simple_session_actons.js
+++ frontend/actions/simple_session_actons.js
@@ -1,2 +1,15 @@
 export const RECEIVE_USER = "RECEIVE_USER";
 export const REMOVE_USER = "REMOVE_USER";
+
+export const receiveUser = (user) => {
+  return({
+    type: RECEIVE_USER,
+    user
+  });
+};
+
+export const removeUser = () => {
+  return({
+    type: REMOVE_USER
+  })
+}
