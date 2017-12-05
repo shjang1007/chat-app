0ac4b344a01004f6b2e2b2b7c196d32a1fc41f04
Brian Jang <brian.jang91@gmail.com>
2017-12-05 18:05:03 -0500

    Add simple session reducer skeleton

--- frontend/reducers/simple_session_reducer.js
+++ frontend/reducers/simple_session_reducer.js
@@ -1,2 +1,10 @@
 // import types
 import { RECEIVE_USER, REMOVE_USER } from "../actions/simple_session_actions";
+
+const _initialState = {};
+
+const simpleSessionReducer = (oldState=_initialState, action) => {
+
+};
+
+export default simpleSessionReducer;
