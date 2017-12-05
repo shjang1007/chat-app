2d91419cee39bad19c963914ad2a4f18320fbd9a
Brian Jang <brian.jang91@gmail.com>
2017-12-05 17:51:11 -0500

    Add skeletal structure for the choose user page

---
+++ frontend/components/start_page/choose_user.jsx
@@ -0,0 +1,26 @@
+// import dependencies
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class ChooseUser extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
+
+const mapStateToProps = (state) => {
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChooseUser);
