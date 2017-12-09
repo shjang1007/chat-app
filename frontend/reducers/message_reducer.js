0ed89fc74873a3aad59748e5117fc89970df18ce
Brian Jang <brian.jang91@gmail.com>
2017-12-09 10:56:34 -0500

    Modify message reducer from object to array and create new delete message method

--- frontend/reducers/message_reducer.js
+++ frontend/reducers/message_reducer.js
@@ -5,22 +5,33 @@ const _initialState = {
 };
 
 const messageReducer = (oldState = _initialState, action) => {
-  // Keep oldState immutable
 
-  let newState;
+  let updatedMessages;
 
   switch (action.type) {
     case RECEIVE_MESSAGES:
-      return {
-        messages: action.messages;
-      }
+
+      return { messages: action.messages; }
+
     case ADD_MESSAGE:
-      newState = Object.assign(oldState, action.message);
-      return newState;
+      updatedMessages = oldState.messages.push(action.message);
+
+      return { messages: updatedMessages };
+
     case DELETE_MESSAGE:
-      newState = Object.assign(oldState, action.message);
-      delete newState[action.messageId];
-      return newState;
+      updatedMessages = oldState.messages;
+      let index;
+      for (let i = 0; i < updatedMessages.length; i++) {
+        const element = updatedMessages[i];
+        if (element.id === action.messageId) {
+          index = i;
+          break
+        };
+      };
+
+      updatedMessages.splice(index, 1);
+
+      return updatedMessages;
     default:
       return oldState;
   }
