1d66d032bbbf04c46ae1647e0e76251ef0d2b0d9
Brian Jang <brian.jang91@gmail.com>
2017-12-10 20:24:22 -0500

    Add skeletal structure for logout button, modular so can use it for any buttons

--- frontend/components/buttons/logout_button.jsx
+++ frontend/components/buttons/logout_button.jsx
@@ -2,3 +2,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+
+// import user actions
+import { removeUser } from "../../actions/simple_session_actions";
+
+const LogoutButton = ({ removeUser }) => {
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeUser: () => dispatch(removeUser())
+  }
+};
+
+export default withRouter(connect(
+  null,
+  mapDispatchToProps
+)(LogoutButton));
